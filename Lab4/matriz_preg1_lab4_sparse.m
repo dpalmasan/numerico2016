719cc21bfbe73c0659b7bfaf93bb4046bd510a01
Diego Palma <dpalmasan@gmail.com>
2016-10-28 14:03:36 -0300

    Cambiada la forma de crear la matriz D
    Se usaba ones(nn), fue cambiado por ones(nn, 1)

--- Lab4/matriz_preg1_lab4_sparse.m
+++ Lab4/matriz_preg1_lab4_sparse.m
@@ -4,6 +4,6 @@ A = aa*ones(nn,1);
 B = bb*ones(nn,1);
 C = cc*ones(nn,1);
 T = spdiags([C A B],-1:1,nn,nn);
-D = dd*spdiags(ones(nn),nn-3,nn,nn);
+D = dd*spdiags(ones(nn, 1),nn-3,nn,nn);
 
 AA = T + D + D';
